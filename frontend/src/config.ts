b58fa958b41424e4e544491f0447d8cfb348fef2
Luciano Bermudez <lucianobmecheng69@gmail.com>
2025-11-15 16:01:13 -0800

    debug

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,5 +1,5 @@
 const rawUrl = import.meta.env.VITE_API_URL ?? "http://localhost:8000";
-
+console.log("This is the raw url", rawUrl)
 export const API_URL = rawUrl.startsWith("http")
   ? rawUrl.replace(/\/$/, "")
   : `https://${rawUrl.replace(/\/$/, "")}`;
