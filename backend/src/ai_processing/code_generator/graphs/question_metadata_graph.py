f3d2f7cdd89af3a2015911903cef9b858cfce90f
Luciano Bermudez <lucianobmecheng69@gmail.com>
2025-11-19 22:07:58 -0800

    added the metadata graph

--- backend/src/ai_processing/code_generator/graphs/question_metadata_graph.py
+++ backend/src/ai_processing/code_generator/graphs/question_metadata_graph.py
@@ -2,6 +2,8 @@ from typing import Annotated, List, Literal, TypedDict
 import operator
 from pathlib import Path
 import json
+from src.ai_processing.code_generator.models.models import question_types
+from pydantic import BaseModel, Field
 
 # --- Project Imports ---
 from src.ai_base.settings import get_settings
@@ -36,8 +38,55 @@ model = init_chat_model(
 )
 
 client = Client()
-base_prompt = client.pull_prompt("server_js_graph_prompt")
+base_prompt = client.pull_prompt("base_metadata")
 if isinstance(base_prompt, str):
     prompt: ChatPromptTemplate = ChatPromptTemplate.from_template(base_prompt)
 else:
     prompt: ChatPromptTemplate = base_prompt
+
+
+class QuestionMetaData(BaseModel):
+    title: str = Field(..., description="A concise title summarizing the question")
+    question_type: question_types
+    topics: List[str] = Field(default=[])
+
+
+class State(TypedDict):
+    question: Question
+    metadata: QuestionMetaData | None
+
+
+def generate_question_metadata(state: State):
+    question_text = state["question"].question_text
+    structured_model = model.with_structured_output(QuestionMetaData)
+    messages = prompt.format_prompt(question=question_text).to_messages()
+    result = structured_model.invoke(messages)
+    result = QuestionMetaData.model_validate(result)
+    return {"metadata": result}
+
+
+workflow = StateGraph(State)
+# Define Nodes
+workflow.add_node("generate_question_metadata", generate_question_metadata)
+# Connect
+workflow.add_edge(START, "generate_question_metadata")
+workflow.add_edge("generate_question_metadata", END)
+
+memory = MemorySaver()
+app = workflow.compile(checkpointer=memory)
+if __name__ == "__main__":
+    config = {"configurable": {"thread_id": "customer_123"}}
+    question = Question(
+        question_text="A car is traveling along a straight rode at a constant speed of 100mph for 5 hours calculate the total distance traveled",
+        solution_text=None,
+        question_solution=None,
+    )
+    input_state: State = {"question": question, "metadata": None}
+    result = app.invoke(input_state, config=config)  # type: ignore
+    print(result["metadata"])
+
+    # Save output
+    output_path = Path(r"src/ai_processing/code_generator/outputs/question_metadata")
+    save_graph_visualization(app, output_path, filename="graph.png")
+    data_path = output_path / "output.json"
+    data_path.write_text(json.dumps(to_serializable(result)))
