f1d8d4901155fa306a49572f83688db87a13be99
Luciano Bermudez <lucianobmecheng69@gmail.com>
2025-12-02 13:16:41 -0800

    added the image endpoint to the frontend api and update the codegenerator ui

--- frontend/src/api/aiWorkspaceAPI.ts
+++ frontend/src/api/aiWorkspaceAPI.ts
@@ -1,3 +1,4 @@
+import { toast } from "react-toastify";
 import { aiApi } from "./client";
 
 export type QuestionDataText = {
@@ -10,5 +11,15 @@ export class AIWorkspaceAPI {
   static async generateText(question: QuestionDataText) {
     const response = await aiApi.post(this.base, question);
     return response.data;
+  }
+  static async generateImage(file: File) {
+    try {
+      const formData = new FormData();
+      formData.append("image", file);
+      const response = await aiApi.post(`${this.base}image`, formData);
+      return response.data;
+    } catch (error) {
+      console.log(error);
+    }
   }
 }
