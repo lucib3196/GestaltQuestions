34efbc18bfe88fb1cf105307b19c4533bf306b17
Luciano Bermudez <lucianobmecheng69@gmail.com>
2025-12-05 12:23:12 -0800

    updated the python runner to handle the new exception

--- code_sandbox/code_sandbox/dummy.py
+++ code_sandbox/code_sandbox/dummy.py
@@ -4,4 +4,7 @@ from code_sandbox.code_runner.javascript_runner import JavaScriptRunner
 if __name__ == "__main__":
     path = Path(r"app_test/assets/generateWrong.js").resolve()
     javascript_runner = JavaScriptRunner()
-    print("result", javascript_runner.run(code=path.read_text(encoding="utf-8", errors="replace")))
+    print(
+        "result",
+        javascript_runner.run(code=path.read_text(encoding="utf-8", errors="replace")),
+    )
