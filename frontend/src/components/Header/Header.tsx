57173da35e44f8b88a7a6910a940e065c81206d3
Luciano Bermudez <lucianobmecheng69@gmail.com>
2025-12-30 13:28:17 -0800

    Added new stye to header to use the old tilte header and updated te authentication page to use it

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,45 +1,46 @@
 import clsx from "clsx";
 import type React from "react";
 
-type HeaderStyle = "Generic" | "QuestionBuilder";
+type HeaderStyle = "Generic" | "QuestionBuilder" | "Authentication";
 
 type HeaderStyleConfig = {
-    wrapper: string;
-    title: string;
+  wrapper: string;
+  title: string;
 };
 
 const styles: Record<HeaderStyle, HeaderStyleConfig> = {
-    Generic: {
-        wrapper: "",
-        title: "truncate text-2xl font-bold text-slate-900 dark:text-slate-100",
-    },
-    QuestionBuilder: {
-        wrapper: "w-full bg-slate-50 flex flex-col px-6 py-4",
-        title:
-            "truncate text-3xl font-semibold text-slate-800 tracking-tight mb-4",
-    },
+  Generic: {
+    wrapper: "",
+    title: "truncate text-2xl font-bold text-slate-900 dark:text-slate-100",
+  },
+  Authentication: {
+    wrapper: "flex flex-col text-black items-center text-center space-y-1",
+    title: "text-xl font-semibold tracking-tight",
+  },
+
+  QuestionBuilder: {
+    wrapper: "w-full bg-slate-50 flex flex-col px-6 py-4",
+    title: "truncate text-3xl font-semibold text-slate-800 tracking-tight mb-4",
+  },
 };
 type HeaderProps = React.HTMLAttributes<HTMLDivElement> & {
-    title: string;
-    children?: React.ReactNode;
-    style?: HeaderStyle;
-    className?: string;
+  title: string;
+  children?: React.ReactNode;
+  style?: HeaderStyle;
+  className?: string;
 };
 export default function Header({
-    title,
-    children,
-    style = "Generic",
-    className,
-    ...rest
+  title,
+  children,
+  style = "Generic",
+  className,
+  ...rest
 }: HeaderProps) {
-    const styleConfig = styles[style as HeaderStyle];
-    return (
-        <div
-            {...rest}
-            className={clsx(styleConfig.wrapper, className)}
-        >
-            <h1 className={styleConfig.title}>{title}</h1>
-            {children}
-        </div>
-    );
+  const styleConfig = styles[style as HeaderStyle];
+  return (
+    <div {...rest} className={clsx(styleConfig.wrapper, className)}>
+      <h1 className={styleConfig.title}>{title}</h1>
+      {children}
+    </div>
+  );
 }
--- frontend/src/pages/AuthenticationPage.tsx
+++ frontend/src/pages/AuthenticationPage.tsx
@@ -4,72 +4,75 @@ import { MyModal } from "../components/Base/MyModal";
 import { useAuth } from "../context/AuthContext";
 import ToggleButton from "@mui/material/ToggleButton";
 import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
-
-import { TitleHeader } from "../components/Base/TitleHeader";
+import { Header } from "../components/Header";
 import VerifyAccount from "../features/UserForm/VerifyAccount";
-import { LogInForm, SignUpForm, } from "../features/UserForm"
+import { LogInForm, SignUpForm } from "../features/UserForm";
 
 // Handles wether the page is show
 type AuthenticationPageModel = {
-    show: boolean;
-    setShow: () => void;
+  show: boolean;
+  setShow: () => void;
 };
 
 export default function AuthenticationPage({
-    show,
-    setShow,
+  show,
+  setShow,
 }: AuthenticationPageModel) {
-    const { mode, setMode } = UseAuthMode();
-    const { user } = useAuth();
+  const { mode, setMode } = UseAuthMode();
+  const { user } = useAuth();
 
-    const handleAlignment = (
-        _: React.MouseEvent<HTMLElement>,
-        state: UserMode | null
-    ) => {
-        if (state !== null) setMode(state);
-    };
+  const handleAlignment = (
+    _: React.MouseEvent<HTMLElement>,
+    state: UserMode | null
+  ) => {
+    if (state !== null) setMode(state);
+  };
 
-    const shouldShowToggle = !user; // Only show toggle if not logged in
-    const showLogin = mode === "login" && (!user);
-    const showSignup = mode === "signup";
-    const showVerify = mode === "authenticate" || (user && !user.emailVerified);
+  const shouldShowToggle = !user; // Only show toggle if not logged in
+  const showLogin = mode === "login" && !user;
+  const showSignup = mode === "signup";
+  const showVerify = mode === "authenticate" || (user && !user.emailVerified);
 
-    return (
-        <Section id="AuthenticationPage">
-            {show && (
-                <MyModal setShowModal={setShow} className="flex flex-col items-center justify-center gap-y-5">
-                    {shouldShowToggle && (
-                        <ToggleButtonGroup
-                            value={mode}
-                            exclusive
-                            onChange={handleAlignment}
-                            aria-label="auth toggle"
-                            className="flex justify-center space-x-3"
-                        >
-                            <ToggleButton value="login" aria-label="login">
-                                Log In
-                            </ToggleButton>
-                            <ToggleButton value="signup" aria-label="signup">
-                                Sign Up
-                            </ToggleButton>
-                        </ToggleButtonGroup>
-                    )}
-                    <TitleHeader
-                        value={
-                            showLogin
-                                ? "Log In"
-                                : showSignup
-                                    ? "Sign Up"
-                                    : showVerify
-                                        ? "Verify Account"
-                                        : ""
-                        }
-                    />
-                    {showLogin && <LogInForm />}
-                    {showSignup && <SignUpForm />}
-                    {showVerify && <VerifyAccount />}
-                </MyModal>
-            )}
-        </Section>
-    );
+  return (
+    <Section id="AuthenticationPage">
+      {show && (
+        <MyModal
+          setShowModal={setShow}
+          className="flex flex-col items-center justify-center gap-y-5"
+        >
+          {shouldShowToggle && (
+            <ToggleButtonGroup
+              value={mode}
+              exclusive
+              onChange={handleAlignment}
+              aria-label="auth toggle"
+              className="flex justify-center space-x-3"
+            >
+              <ToggleButton value="login" aria-label="login">
+                Log In
+              </ToggleButton>
+              <ToggleButton value="signup" aria-label="signup">
+                Sign Up
+              </ToggleButton>
+            </ToggleButtonGroup>
+          )}
+          <Header
+            style={"Authentication"}
+            title={
+              showLogin
+                ? "Log In"
+                : showSignup
+                ? "Sign Up"
+                : showVerify
+                ? "Verify Account"
+                : ""
+            }
+          />
+          {showLogin && <LogInForm />}
+          {showSignup && <SignUpForm />}
+          {showVerify && <VerifyAccount />}
+        </MyModal>
+      )}
+    </Section>
+  );
 }
