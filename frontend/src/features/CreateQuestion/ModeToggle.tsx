eb1bc8bc7492f19d786a2f6157b33d9e79436288
Luciano Bermudez <lucianobmecheng69@gmail.com>
2026-01-09 13:28:39 -0800

    Fixed imports

--- frontend/src/features/CreateQuestion/ModeToggle.tsx
+++ frontend/src/features/CreateQuestion/ModeToggle.tsx
@@ -1,6 +1,7 @@
 import ToggleButton from "@mui/material/ToggleButton";
 import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
-import { useCreateMode, type CreateMode } from "./context";
+import { useCreateMode } from "./context";
+import type { CreateMode } from "./types";
 
 export default function ModeToggle() {
     const { mode, setMode } = useCreateMode()
