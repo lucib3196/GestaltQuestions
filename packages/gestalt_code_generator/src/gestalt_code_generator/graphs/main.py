c03a8fe9a8764fe1641d7fcd00fa0dd176043ccb
Luciano Bermudez <lberm007@ucr.edu>
2026-06-26 14:44:14 -0700

    Added general context for the langchain agents allowing for dynamic model change

---
+++ packages/gestalt_code_generator/src/gestalt_code_generator/graphs/main.py
@@ -0,0 +1,18 @@
+from langchain.chat_models import init_chat_model
+
+print("Running")
+# Initialize a configurable model
+model = init_chat_model(temperature=0, model_provider="google_genai",configurable_fields=("model",))
+
+# Invoke with different models at runtime
+response_1 = model.invoke(
+    "what's your name",
+    config={"configurable": {"model": "gemini-2.5-flash"}},
+)
+response_2 = model.invoke(
+    "what's your name",
+    config={"configurable": {"model": "gemini-2.5-flash"}},
+)
+
+print(response_1)
+print(response_2)
